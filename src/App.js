486f159c68c50cdefcc1087610be226bd92f25a3
Paul Chávez <paulchavezromero20@gmail.com>
2020-11-28 00:24:32 -0500

    ModalContainer component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container, Snackbar } from '@material-ui/core';
 import Header from './components/Header';
 import SendTweet from './components/SendTweet';
 
+
 function App() {
 	return (
 		<Container className="tweets-simulator" maxWidth={ false }>
