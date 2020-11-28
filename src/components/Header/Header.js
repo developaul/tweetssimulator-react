25366b59dc6a0059da88d794310319b4bd6504de
Paul Chávez <paulchavezromero20@gmail.com>
2020-11-28 00:55:00 -0500

    FormSendTweet component added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,11 @@ import React from 'react';
 import TwitterLogo from '../../assets/img/twitter-logo.png';
 import './Header.scss';
 
-const Header = () => {
-    return (
-        <div className="header">
-            <img src={ TwitterLogo } alt="Tweets Simulator" />
-            <h1>Tweets Simulator</h1>
-        </div>
-    );
-};
+const Header = () => (
+    <div className="header">
+        <img src={ TwitterLogo } alt="Tweets Simulator" />
+        <h1>Tweets Simulator</h1>
+    </div>
+);
 
 export default Header;
